d21be9b3a834f67cbca15048e24e3b57d6e64822
TAN <TAN@164.107.236.22>
2017-10-21 11:23:42 -0400

    BinaryTree set up

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,10 +1,15 @@
+import java.util.Map;
 
 public class BinaryTree implements BinaryTreeInterface {
 	
-	public class Node() {
+	private Node node;
+	private BinaryTree left;
+	private BinaryTree right;
+	private int path;
+	
+	public BinaryTree(Map m) {
 		
 	}
-
 
 	@Override
 	public void assemby(BinaryTreeInterface left, BinaryTreeInterface right) {
@@ -22,5 +27,15 @@ public class BinaryTree implements BinaryTreeInterface {
 	public int getPath(BinaryTreeInterface.Node n) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public class Node{
+		final int count;
+		final char c;
+		
+		public Node(int count, char c) {
+			this.count = count;
+			this.c = c;
+		}
 	}
 }
