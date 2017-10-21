def13c938cb7c969ddc15717ae02667c3b19733c
TAN <TAN@164.107.236.22>
2017-10-21 11:23:25 -0400

    BinaryTree

---
+++ src/BinaryTree.java
@@ -0,0 +1,26 @@
+
+public class BinaryTree implements BinaryTreeInterface {
+	
+	public class Node() {
+		
+	}
+
+
+	@Override
+	public void assemby(BinaryTreeInterface left, BinaryTreeInterface right) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getNode(int binary) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getPath(BinaryTreeInterface.Node n) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
