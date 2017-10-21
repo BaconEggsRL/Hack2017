eb989798496a4a5caebd22bc8fb88c50173beea9
TAN <TAN@164.107.236.22>
2017-10-21 11:13:03 -0400

    BT interface

---
+++ src/BinaryTreeInterface.java
@@ -0,0 +1,13 @@
+
+
+public interface BinaryTreeInterface {
+	
+	public interface Node {
+		
+
+	}
+	public void assemby(BinaryTreeInterface left, BinaryTreeInterface right);
+	public void getNode(int binary);
+	public int getPath(Node n);
+	
+}
